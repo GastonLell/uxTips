f744a197826e15a532e88420247cd80b04d3e1de
GastonLell <gastonglell@gmail.com>
2020-11-26 00:48:25 -0300

    copia pagina uxTips

---
+++ script.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+
+showSlides();
+
+function showSlides() {
+
+    let slides = document.getElementsByClassName("mySlides");
+    let i;
+
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    } 
+    
+    slideIndex++;
+
+    if(slideIndex > slides.length) {slideIndex = 1}
+
+    slides[slideIndex -1].style.display = "block";
+
+    setTimeout(showSlides,10000);
+
+}
+
